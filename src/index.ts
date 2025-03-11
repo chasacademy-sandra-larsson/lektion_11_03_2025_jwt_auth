a9fc6028f0cb473ac857fbce630ef5f57c3b80a2
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-03-11 11:11:32 +0100

    added routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
 import cors from "cors";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express();
 
@@ -7,9 +10,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Node.js and Express.js with TypeScript");
-});
+
+app.post("/sign-up", (req: Request, res: Response) => {
+
+  // Läs in användares email och lösenord
+
+  // Kryptera lösenordeet innan det sparas i DB
+
+})
+
+
+app.post("/sign-in", (req: Request, res: Response) => {
+
+  // Hämta användare i databasen utifrån email
+
+  // Kolla om det inkommande lösenordet är samma som användaren i databasen 
+
+      // Skapas en JWT - JSON Web Token
+
+      // Skicka token till klienten
+
+})
+
+// Middleware 
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+
+  // Kolla om token är valid
+
+}
+
+
+app.get("/dashboard", authMiddleware, (req: Request, res: Response) => {
+
+})
+
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
